6b94012b80d356b6a0a30fd998f08dfe8eca60bc
Mads Nicolaj Nielsen <45572200+madsen372@users.noreply.github.com>
2023-01-26 10:09:52 +0100

    Update theme.config.js

--- theme.config.js
+++ theme.config.js
@@ -4,11 +4,8 @@ export default {
   footer: (
     <small style={{ display: 'block', marginTop: '8rem' }}>
       <time>{YEAR}</time> © Mads Nicolaj Nielsen.
-      <a href="/feed.xml">RSS</a>
       <style jsx>{`
-        a {
-          float: right;
-        }
+
         @media screen and (max-width: 480px) {
           article {
             padding-top: 2rem;
